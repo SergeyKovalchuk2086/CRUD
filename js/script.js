bb1d0611bd1ec53ba18355835126ba4296326328
Sergey Kovalchuk <kovalchuk.s.v@inbox.ru>
2021-09-30 17:56:40 +0700

    add some js

--- js/script.js
+++ js/script.js
@@ -3,16 +3,18 @@ const inputSurname = document.querySelector('.inputSurname');
 const inputPost = document.querySelector('.inputPost');
 const addBtn = document.querySelector('.addBtn');
 // const deleteBtn = document.querySelector('.deleteBtn');
-const tableBody = document.querySelector('.tableBody');
+let tbody = document.querySelector('tbody');
 
 addBtn.addEventListener('click', addPerson);
 
 let personList = [];
 
-
+if (localStorage.getItem('person')) {
+	personList = JSON.parse(localStorage.getItem('person'));
+	displayPerson();
+}
 
 function addPerson() {
-
 	let person = {
 		number: personList.length + 1,
 		name: inputName.value,
@@ -22,77 +24,61 @@ function addPerson() {
 	}
 
 	personList.push(person);
-	console.log(personList);
+	localStorage.setItem('person', JSON.stringify(personList));
+
 	displayPerson();
 	inputName.value = '';
 	inputSurname.value = '';
 	inputPost.value = '';
-
-	// btns()
-
+	initDeleteBtn();
+	initEditBtn();
 }
-
-// function btns() {
-// const deleteBtn = document.getElementById('deleteBtn');
-// console.log(deleteBtn);
-
-tableBody.addEventListener('click', function (event) {
-	// console.log(event.target.parentNode);
-	// })
-	// deleteBtn.addEventListener('click', function (event) {
-	// 	console.log(event);
-	let parentDeleteBtn = event.target.parentNode;
-	let parent = parentDeleteBtn.parentElement;
-	let child = parent.querySelector('.person__name');
-	let childName = child.innerHTML;
-	console.log(childName);
-
-	personList.forEach(function (item, i) {
-		if (childName === item.name) {
-			personList.splice(i, 1)
-		} else {
-			console.log('not deleted');
-		}
-
-
-	})
-
-	displayPerson()
-
-})
-
-// const delItem = document.querySelector('.person__1');
-// console.log(delItem.id);
-
-
-// delItem.addEventListener('click', function(event) {
-//     let www = delItem.c
-//     console.log(event.target.classList.value);
-
-
-// });
-
 
 function displayPerson() {
 	let personInfo = '';
-	debugger
-	personList.forEach(function (item) {
-		personInfo = `
-      	<tr class="person__${item.number}" id="person__${item.number}">
-            <td class="list__number">${item.number}</td>
+	if ( personList.length === 0 ) tbody.innerHTML = '';
+
+	personList.forEach(function(item, i){
+		personInfo +=
+		`<tr class="person__${i+1}">
+            <td class="list__number">${i + 1}</td>
             <td class="person__name">${item.name}</td>
             <td class="person__surname">${item.surname}</td>
-            <td class="person__id" id="q123">${item.number}</td>
-            <td class="person__post">${item.post}</td>
-            <td class="delBtn__${item.number}"><input type="button" value="Delete" class="deleteBtn" id="deleteBtn"></td>
-            <td><input type="button" value="Edit"></td>
-      	</tr>
-      	`
-	});
+			<td class="person__post">${item.post}</td>
+            <td class="person__id">${i+1}</td>
+			<td class="deleteBtn" id="deleteBtn">Delete</td>
+			<td class="editBtn" id="editBtn">Edit</td>
+        </tr>`
 
-	if (personList.length === 0) {
-		tableBody.insertAdjacentHTML("beforeend", )
-	} else {
-		tableBody.insertAdjacentHTML("beforeend", personInfo);
-	}
+		tbody.innerHTML = personInfo;
+	})
 }
+
+function initDeleteBtn() {
+	let idPerson = ''
+
+	tbody.addEventListener('click', function(event){
+		if (event.target.classList.contains('deleteBtn')) {
+			idPerson = Number(event.target.previousElementSibling.innerHTML);
+		} else {
+			console.log('ne etot class');
+		}
+
+		personList.forEach(function(item, i){
+			if (item.id === idPerson) {
+				personList.splice(i, 1);
+			}
+		});
+
+		displayPerson();
+		localStorage.setItem('person', JSON.stringify(personList));
+	});
+}
+
+function initEditBtn(){
+	tbody.addEventListener('click', function(event){
+		if (event.target.classList.contains('editBtn')) {
+			console.log('edit btn');
+		}
+	});
+};
